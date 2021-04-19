5aa67d99dd2c020d17714bab95d6f0fe42e52e88
GeogeKhodaev <74020503+GeogeKhodaev@users.noreply.github.com>
2021-04-19 18:02:03 +0300

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackgroundImage = global::WinFormsApp1.Properties.Resources.picture1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(326, 487);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 114);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.button1_KeyUp);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.button2.Location = new System.Drawing.Point(354, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 201);
+            this.button2.TabIndex = 2;
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(883, 597);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Timers;
+
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+
+        }
+        public void ImageBox_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+        private void pictureBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                button2.Location = new Point(button2.Location.X - 10, button2.Location.Y);
+                button1.Location = new Point(button1.Location.X - 10, button1.Location.Y);
+            }
+
+            else if (e.KeyCode == Keys.Right)
+            {
+                button2.Location = new Point(button2.Location.X + 10, button2.Location.Y);
+                button1.Location = new Point(button1.Location.X + 10, button1.Location.Y);
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                button2.Visible = true;
+            }
+        }
+
+        private void button1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                button2.Location = new Point(button2.Location.X - 10, button2.Location.Y);
+                button1.Location = new Point(button1.Location.X - 10, button1.Location.Y);
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                button2.Location = new Point(button2.Location.X + 10, button2.Location.Y);
+                button1.Location = new Point(button1.Location.X + 10, button1.Location.Y);
+            }
+            else if(e.KeyCode==Keys.Space)
+            {
+                button2.Visible = false;
+            }
+        }
+
+        Button button;
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            Random r = new Random();
+            button = new Button();
+            button.Location = new Point(r.Next(16, 760), 0);
+            button.BackgroundImage = WinFormsApp1.Properties.Resources.Meteor;
+            button.BackgroundImageLayout = ImageLayout.Zoom;
+
+        }     
+
+       public void timer2_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+
+
